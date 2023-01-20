d8e6e63786bbbae1571a42a7b0b016f24751d014
mosguinz <mos.guinz@gmail.com>
2023-01-20 00:29:13 -0800

    Wrap injection calls for scrapers

--- src/scraper.ts
+++ src/scraper.ts
@@ -1,18 +1,31 @@
 import { Event, League } from './types';
 
-function getEvents(): Event[] {
+function _getEvents(): Event[] {
     const element: any = document.querySelector("div.Event");
     const events: Event[] = element?._component?.props?.schedule?.events;
     console.log(events);
     return events
 }
 
-function getLeagues(): League[] {
+function _getLeagues(): League[] {
     const element: any = document.querySelector("div.EventFilter");
     const leagues: League[] = element?._component?.props?.leagues;
     console.log(leagues);
     return leagues;
 }
 
-getEvents();
-getLeagues();
+export async function getEvents(tab: chrome.tabs.Tab) {
+    return await chrome.scripting.executeScript({
+        target: { tabId: tab.id! },
+        func: _getEvents,
+        world: "MAIN"
+    });
+}
+
+export async function getLeagues(tab: chrome.tabs.Tab) {
+    return await chrome.scripting.executeScript({
+        target: { tabId: tab.id! },
+        func: _getLeagues,
+        world: "MAIN"
+    });
+}
