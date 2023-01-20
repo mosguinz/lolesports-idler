ced358888f685d62181f735194360729ef46c346
mosguinz <mos.guinz@gmail.com>
2023-01-20 01:59:27 -0800

    Remove redudant content script

--- src/content_script.ts
+++
@@ -1,5 +0,0 @@
-var s = document.createElement('script');
-s.src = chrome.runtime.getURL('scraper.js');
-s.onload = () => { s.remove(); };
-
-(document.head || document.documentElement).appendChild(s);
