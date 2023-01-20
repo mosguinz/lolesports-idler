3f1692361093c60f59703fc64ebd90900381e30d
mosguinz <mos.guinz@gmail.com>
2023-01-19 23:28:28 -0800

    Add script to launch schedule page

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,33 @@
+import { AppConfig, AppSession } from "./types";
+
+async function openSchedulePage() {
+    const tab = await chrome.tabs.create({
+        url: "https://lolesports.com/schedule"
+    });
+    const session: AppSession = {
+        scheduleTab: tab,
+        spawnedTabs: []
+    }
+    await chrome.storage.session.set({ "appSession": session });
+}
+
 chrome.action.onClicked.addListener((tab) => {
     console.log("test");
+});
+
+// This listener's only purpose is to start the idler.
+chrome.storage.onChanged.addListener(async (changes, areaName) => {
+    const configChanges = changes?.config;
+    if (!configChanges) {
+        return;
+    }
+
+    const newValue: AppConfig = configChanges?.newValue;
+    const oldValue: AppConfig = configChanges?.oldValue;
+    if (newValue?.enabled === oldValue?.enabled) {
+        return;
+    }
+    if (newValue.enabled) {
+        await openSchedulePage();
+    }
 });
